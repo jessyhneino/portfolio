8b3c1340dab23a5434e12f3d19de8e928e9fe91a
jessy hneino <jessyhneino@gmail.com>
2025-12-06 08:59:43 -0800

    updated2

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,14 +6,14 @@ import CV from "../../assets/cv.pdf";
 function Home() {
   return (
     <div className="w-[100%] p-[40px] sm:p-[60px] md:p-[100px] overflow-hidden" id="home">
-      <div className="w-[100%] md:w-[85%] lg:w-[80%] mx-auto h-full relative contents">
+      <div className="w-[100%] md:w-[85%] lg:w-[80%] mx-auto h-full relative lg:contents">
 
         {/* layout: split left + right */}
         <div className="w-[100%] pt-[96px] flex flex-col-reverse justify-center md:flex-row items-center gap-10">
 
           {/* LEFT SIDE – TEXT */}
           <motion.div
-            className="flex-1 text-center md:text-left ml-[20px]"
+            className="flex-1 text-center md:text-left lg:ml-[20px]"
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: false, amount: 0.3 }}
@@ -81,7 +81,7 @@ function Home() {
                 overflow-hidden
                 flex
                 justify-center
-                mr-[45px]
+                lg:mr-[45px]
               "
             >
               <img className="object-cover h-full w-[80%] ml-[18px]" src={Me} alt="" />
